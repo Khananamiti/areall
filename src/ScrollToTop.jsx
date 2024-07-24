c33304c4c15995912274fe1b2a53bc5fc51fee8b
Khananamiti <khananova_kha@mail.ru>
2024-07-24 18:02:21 +0600

    Начато описание стиля кнопки поднятия вверх

--- src/ScrollToTop.jsx
+++ src/ScrollToTop.jsx
@@ -4,22 +4,23 @@ import { MdKeyboardDoubleArrowUp } from "react-icons/md";
 const ScrollToTop = () => {
   const [showTopBtn, setShowTopBtn] = useState(false); // useState хранит флаг false
 
-  const goToTop = () => { // 
-    window.scrollTo({
+  const goToTop = () => { // Создали функцию
+    //
+    window.scrollTo({ // Обращаемся к окну браузера
       top: 0, // Скролл до верхней точки
       behavior: "smooth", // Плавное перемещение
     });
   };
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      window.scrollY > 400 ? setShowTopBtn(true) : setShowTopBtn(false) // Расстояние от верхней точки больше 400 - флаг появится
-    })
-  }, [])
+      window.scrollY > 400 ? setShowTopBtn(true) : setShowTopBtn(false); // Расстояние от верхней точки больше 400 - флаг появится
+    });
+  }, []);
   return (
     <>
-      {showTopBtn && // Проверяем, если флаг присутствует, то можем на него нажать
-      <MdKeyboardDoubleArrowUp className="btn-up" onClick={goToTop}/>
-      }
+      {showTopBtn && ( // Проверяем, если флаг присутствует, то можем на него нажать
+        <MdKeyboardDoubleArrowUp className="btn-up" onClick={goToTop} /> // ToDo Напиши стиль для кнопки вверх
+      )}
     </>
   );
 };
