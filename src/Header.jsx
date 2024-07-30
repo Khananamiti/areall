662654a95a7598afc46094e63367e22fd52603d2
Khananamiti <khananova_kha@mail.ru>
2024-07-30 17:40:57 +0600

    Попытка реализации копирования текста при нажатии на номер телефона

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,6 @@
 import images from "./constants/data";
 import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const Header = () => {
   const [isActive, setIsActive] = useState(false);
@@ -11,6 +12,14 @@ const Header = () => {
 
   const scrollToSection = (sectionId) => {
     document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+  };
+
+  const [text, setText] = useState("+79136236787");
+  const [isCopied, setIsCopied] = useState(false);
+
+  const onCopyText = () => {
+    setIsCopied(true);
+    setTimeout(() => setIsCopied(false), 1500);
   };
 
   return (
@@ -124,9 +133,16 @@ const Header = () => {
             {/* Реализовать копирование при нажатии на текст */}
             <div className="header__cont-text">
               <div className="text-right">
-                <a href="tel:+79136236787" className="">
-                  +7 (913) 623-67-87
-                </a>
+                <input
+                  value={text}
+                  onChange={({ target }) => setText(target.value)}
+                />
+                <CopyToClipboard text={text} onCopy={onCopyText}>
+                  <button>+7 (913) 623-67-87</button>
+                </CopyToClipboard>
+                {isCopied ? (
+                  <span style={{ color: "green" }}>Скопировано!</span>
+                ) : null}
               </div>
               <div className="text-right">
                 <a href="mailto:areall@mail.ru" className="">
