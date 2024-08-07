49ba4ee7597555d96b6488807e45271a5bdc00e3
Khananamiti <khananova_kha@mail.ru>
2024-08-07 12:15:41 +0600

    Применено замечание "Если вы собираетесь использовать URL этого актива, используйте /icon.jpg?url."

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Header from "./pages/Header";
 import Registration from "./pages/Registration";
 import Authorization from "./pages/Authorization";
-import Library from "./Pages/Library";
+import Library from "./pages/Library";
 import Investigation from "./pages/Investigation";
 import Analysis from "./pages/Analysis";
 import Research from "./pages/Research";
 import Geolocation from "./pages/Geolocation";
-import Footer from "./Pages/Footer";
+import Footer from "./pages/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
